0bfc691f46e89f8b5891173e43e6d47e20c59c35
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-16 16:37:00 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  i = 0
+  new_array = []
+  
+  while i < array.length
+    new_array << yield(array[i])
+    i += 1
+  end
+  new_array
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
